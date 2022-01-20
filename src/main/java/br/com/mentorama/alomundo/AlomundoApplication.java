cb685d44b5f38989960239077a9779d1a8bf5b16
Wesly <weslyramalho@yahoo.com.br>
2022-01-20 20:18:54 -0300

    Projeto criado

---
+++ src/main/java/br/com/mentorama/alomundo/AlomundoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.alomundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlomundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mentorama/alomundo/AlomundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.alomundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlomundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
